b1cd49ad8a21458e6add1c6a7be38d57813bfab4
Rich Mikan <richmikan@richlab.org>
2016-07-14 09:27:15 +0900

    Update mkzipdic_jigyosyo.sh

--- data/mkzipdic_jigyosyo.sh
+++ data/mkzipdic_jigyosyo.sh
@@ -2,9 +2,9 @@
 
 #####################################################################
 #
-# MKZIPDIC_KENALL.SH
-# 日本郵便公式の郵便番号住所CSVから、本システム用の辞書を作成（地域名）
-# Written by Rich Mikan(richmikan[at]richlab.org) at 2016/07/14
+# MKZIPDIC_JIGYOSYO.SH
+# 日本郵便公式の郵便番号住所CSVから、本システム用の辞書を作成（事業所名）
+# Written by Rich Mikan(richmikan[at]richlab.org) at 2014/07/14
 #
 # Usage : mkzipdic.sh -f
 #         -f ... ・サイトにあるCSVファイルのタイプスタンプが、
@@ -25,8 +25,8 @@
 
 # --- 変数定義 -------------------------------------------------------
 dir_MINE="$(d=${0%/*}/; [ "_$d" = "_$0/" ] && d='./'; cd "$d"; pwd)" # このshのパス
-readonly file_ZIPDIC="$dir_MINE/ken_all.txt"                         # 郵便番号辞書ファイルのパス
-readonly url_ZIPCSVZIP=http://www.post.japanpost.jp/zipcode/dl/oogaki/zip/ken_all.zip
+readonly file_ZIPDIC="$dir_MINE/jigyosyo.txt"                        # 郵便番号辞書ファイルのパス
+readonly url_ZIPCSVZIP=http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip
                                                                      # 日本郵便 郵便番号-住所 CSVデータ (Zip形式) URL
 readonly flg_SUEXECMODE=0                                            # サーバーがsuEXECモードで動いているなら1
 
@@ -110,6 +110,7 @@ while [ $flg_FORCE -eq 0 ]; do
   # その辞書ファイル内にタイムスタンプは記載されているか?
   timestamp_local=$(head -n 1 "$file_ZIPDIC" | awk '{print $NF}')
   printf '%s\n' "$timestamp_local" | grep -Eq '^[0-9]{14}$' || break
+  [ $? -eq 0 ] || break
   # サイト上のファイルは手元のファイルよりも新しいか?
   [ $timestamp_web -gt $timestamp_local ] && break
   # そうでなければ何もせず終了(正常)
@@ -143,20 +144,19 @@ else                                                              #
   error_exit 9 'No KANJI convertors found (iconv or nkf)'         #
 fi                                                                |
 # 日本郵便 郵便番号-住所 CSVデータ(UTF-8変換済)                   #
-$dir_MINE/parsrc.sh                                             | # CSVパーサー(自作コマンド)
+$dir_MINE/../commands/parsrc.sh                                   | # CSVパーサー(自作コマンド)
 # 1:行番号 2:列番号 3:CSVデータセルデータ                         #
-awk '$2~/^3|7|8|9$/'                                              |
-# 1:行番号 2:列番号(3=郵便番号,7=都道府県,8=市区町村,9=町) 3:データ
+awk '$2~/^8|4|5|6|7$/'                                            |
+# 1:行番号 2:列番号(8=郵便番号,4=都道府県,5=市区町村,6-7=町) 3:データ
 awk 'BEGIN{z="#"; p="generated"; c="at"; t="'$timestamp_web'"; }  #
      $1!=line      {pl();z="";p="";c="";t="";line=$1;          }  #
-     $2==3         {z=$3;                                      }  #
-     $2==7         {p=$3;                                      }  #
-     $2==8         {c=$3;                                      }  #
-     $2==9         {t=$3;                                      }  #
+     $2==8         {z=$3;                                      }  #
+     $2==4         {p=$3;                                      }  #
+     $2==5         {c=$3;                                      }  #
+     $2==6         {t=$3;                                      }  #
+     $2==7         {t2=$3;                                     }  #
      END           {pl();                                      }  #
-     function pl() {print z,p,c,t;                             }' |
-sed 's/（.*//'                                                    | # 地域名住所文字列で小括弧以降は使えないので除去する
-sed 's/以下に.*//'                                                > $tmpf_zipdic # 「以下に」の場合も同様
+     function pl() {print z,p,c,t t2;                          }' > $tmpf_zipdic
 # 1:郵便番号 2:都道府県名 3:市区町村名 4:町名
 [ -s $tmpf_zipdic ] || error_exit 10 'Failed to make the zipcode dictionary file'
 mv $tmpf_zipdic "$file_ZIPDIC"
